1acb21ac498adf11932227fdb50343329e7f00f0
Radi Ahmed Chowdhury <chyradi.2000@gmail.com>
2025-07-22 13:18:35 +0600

    Update database for project.sql

--- database for project.sql
+++ database for project.sql
@@ -1,11 +1,11 @@
 create database bankmanagementsystem;
 show databases;
 use bankmanagementsystem;
-create table signup(formno varchar(20), name varchar(20), father_name varchar(20), dob varchar(20), gender varchar(20),email varchar(30), marital_status varchar(20), address varchar(40), city varchar(25), pincode varchar(20), state varchar(25));
+create table signup(formno varchar(20), name varchar(20), father_name varchar(20), dob varchar(20), gender varchar(20),email varchar(30), marital_status varchar(20), address varchar(40), upazila varchar(25), postcode varchar(20), state varchar(25));
 show tables;
 select * from signup;
 
-create table signuptwo(formno varchar(20), religion varchar(20), category varchar(20), income varchar(20), education varchar(20), occupation varchar(20), pan varchar(20), aadhar varchar(20), seniorcitizen varchar(20), existingaccount varchar(20));
+create table signuptwo(formno varchar(20), religion varchar(20), category varchar(20), income varchar(20), education varchar(20), occupation varchar(20), br varchar(20), nid varchar(20), seniorcitizen varchar(20), existingaccount varchar(20));
 
 select * from signuptwo;
 
